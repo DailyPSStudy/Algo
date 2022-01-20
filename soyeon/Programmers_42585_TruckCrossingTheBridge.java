78cf81d68fd36f60b1d447769daf27ffff1b9df9
soyeon <dtd1170@naver.com>
2022-01-20 12:09:06 +0900

    2022-01-20 수정

--- soyeon/Programmers_42585_TruckCrossingTheBridge.java
+++ soyeon/Programmers_42585_TruckCrossingTheBridge.java
@@ -1,50 +1,74 @@
 package programmers_42585_TruckCrossingTheBridge;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import programmers_42585_TruckCrossingTheBridge.Solution.Truck;
 
 public class Programmers_42585_TruckCrossingTheBridge {
-	class Truck{
-		int weight;
-		int startSec;
-
-		public Truck(int weight, int startSec) {
-			this.weight = weight;
-			this.startSec = startSec;
-		}
+	static class Truck {
+        int time;
+        int weight;
+        public Truck(int time, int weight) {
+            this.weight = weight;
+            this.time = time;
+        }
+    }
+    
+    public static void main(String[] args) {
+		int ans = solution(100, 100, new int[] {10,10,10,10,10,10,10,10,10,10});
+		System.out.println(ans);
 	}
-	
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        /** bridge_length 사이즈 큐 */
-        Queue<Truck> load = new LinkedList<>();
-        int sec = 0; // 초
-        int trIdx = 0; // 트럭 인덱스
-        int curWeight = 0; // 현재 무게
+    
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        Deque<Truck> bridge = new LinkedList<>();
+        Queue<Integer> wait = new LinkedList<>();
+        // array -> queue
+        wait = putInQueue(truck_weights);
+        // System.out.println(wait.size());
+        int time = 0;
+        int curWeight = 0;
+        // 도로 지나는데 1초 걸리는거냐 길이 상관없이 ...?
+        do {
+            System.out.printf("%d초\n", time);
+            
+            // bridge_length초만큼 지났으면 큐에서 빼기, sec초기화
+            if(bridge.size()!=0 && bridge.peek().time+bridge_length==time) {
+                Truck truck = bridge.pollFirst();
+                int out = truck.weight;
+                System.out.printf("%d 빠져나감\n", out);
+                curWeight -= out;
+            }
+            
+            // bridge에 아무것도 없으면 넣기
+            if(bridge.size()==0 && wait.size()!=0) {
+                int in = wait.poll();
+                System.out.printf("도로에 아무것도 없으므로 %d 들어감\n", in);
+                bridge.add(new Truck(time, in));
+                curWeight += in;
+            }
+            // wait.peek + bridge.pollLast 가 weight보다 작으면 넣기
+            else if(wait.size()!=0 && wait.peek()+curWeight <= weight) {
+                int in = wait.poll();
+                bridge.addLast(new Truck(time, in));
+                curWeight += in;
+                System.out.printf("%d 들어감\n", in);
+            }
+            
+            time++;
+        } while(bridge.size()!=0); // end of while
         
-        while(true) {
-        	/** 0초(맨처음)가 아닌데 큐가 비어있으면 모든 트럭이 다 지나간 것이므로 break */
-        	if(load.isEmpty() && sec != 0) break;
-        	sec++;
-        	
-        	/** 트럭이 도로 위에 다리길이 초만큼 있었으면 이미 지나간것이므로 도로(큐)에서 빼낸다 */
-        	if(!load.isEmpty() && sec - load.peek().startSec == bridge_length) { // 초기상태일때도 load는 비어있으므로 조건체크 해준다.
-        		curWeight -= load.peek().weight; // 도로에서 빠져나갔으므로 현재 무게에서도 빼냄
-        		load.poll();
-        	}
-        	
-        	/** 현재 다리 위에 제한무게이하의 트럭들이 있으면 도로에 트럭을 더해준다. */
-        	if(load.size() < bridge_length && trIdx < truck_weights.length) {
-        		if(!(trIdx > truck_weights.length - 1)) { // trIdx가 범위초과하지 않는다면
-        			if(curWeight + truck_weights[trIdx] <= weight) { // 다음 트럭이 도로에 올라왔을 시 최대무게를 넘어가지 않는지 체크
-        				load.add(new Truck(truck_weights[trIdx], sec));
-        				curWeight += truck_weights[trIdx];
-        				trIdx++;
-        			}
-        		}
-        	}
-        } // end of while
-        return sec;
+        return time;
+        
+    } // end of solution
+    
+    
+    public static Deque<Integer> putInQueue(int[] weights) {
+        Deque<Integer> res = new LinkedList<>();
+        for(int i = 0; i < weights.length; i++) {
+            res.add(weights[i]);
+        }
+        return res;
     }
 }
